b31db1eb5db7abac58d4d07d5cb328040393fbe6
shiika89 <kazuki_plant@yahoo.co.jp>
2021-05-22 23:25:18 +0900

    セルとBingoの元となるScriptを追加

---
+++ Assets/Bingo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bingo : MonoBehaviour
+{
+    [SerializeField] private Cell m_cellPrefab = null;
+    [SerializeField] private GridLayoutGroup m_gridLayoutGroup = null;
+    [SerializeField] int m_indexNumX = 5;
+    [SerializeField] int m_indexNumY = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_indexNumX < m_indexNumY)
+        {
+            m_gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+            m_gridLayoutGroup.constraintCount = m_indexNumX;
+        }
+        else
+        {
+            m_gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedRowCount;
+            m_gridLayoutGroup.constraintCount = m_indexNumY;
+        }
+
+        //cubes = new GameObject[m_indexNumX, m_indexNumY];
+        var cubes = new Cell[m_indexNumX, m_indexNumY];
+        for (int i = 0; i < m_indexNumX; i++)
+        {
+            for (int x = 0; x < m_indexNumY; x++)
+            {
+                var cell = Instantiate(m_cellPrefab);
+                var parent = m_gridLayoutGroup.gameObject.transform;
+                cell.transform.SetParent(parent);
+                //cell.transform.position = new Vector3(-4 + i * 2, -3 + x * 2, 0);
+                cubes[i, x] = cell;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
